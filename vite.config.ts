891ca21718b5bf0dda667701e8db6490ba966335
Lucas Eduardo de Paula <117169607+LucasdePaula84@users.noreply.github.com>
2025-11-29 08:01:37 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, (process as any).cwd(), '');
   return {
     plugins: [react()],
     define: {
-      'process.env.API_KEY': JSON.stringify(env.API_KEY)
+      // O || "" evita que o app trave se a chave estiver vazia
+      'process.env.API_KEY': JSON.stringify(env.API_KEY || "")
     }
   };
 });
